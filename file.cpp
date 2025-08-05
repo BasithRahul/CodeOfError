11ade734b04aba44a6aa9b596a65b1c013e3f388
Basith Rahul <basithrahul34@gmail.com>
2025-08-05 11:38:24 +0530

    Fix division by zero error by adding a conditional check

--- file.cpp
+++ file.cpp
@@ -4,7 +4,12 @@ using namespace std;
 int main() {
     int a = 5;
     int b = 0;
-    int result = a / b; // This line will cause a division by zero error
-    cout << "Result: " << result << endl;
+    int result = 0;
+    if (b != 0) {
+        result = a / b;
+        cout << "Result: " << result << endl;
+    } else {
+        cout << "Error: Division by zero is not allowed." << endl;
+    }
     return 0;
 }
